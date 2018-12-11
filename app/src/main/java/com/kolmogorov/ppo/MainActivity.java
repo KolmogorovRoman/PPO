2746816b3e27aafd8c554174a5b9aa8d5347817e
Force <force12378@gmail.com>
2018-12-11 21:23:54 +0300

    Updated

--- app/src/main/java/com/kolmogorov/ppo/MainActivity.java
+++ app/src/main/java/com/kolmogorov/ppo/MainActivity.java
@@ -8,7 +8,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.widget.TextView;
-
 import java.util.jar.Manifest;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,7 +21,6 @@ public class MainActivity extends AppCompatActivity {
         java.lang.String imei="qwe";
         if (PermissionChecker.checkSelfPermission(this, "android.permission.READ_PHONE_STATE") == PermissionChecker.PERMISSION_GRANTED)
             imei=tm.getDeviceId();
-
 
         tw.setText(imei);
     }
